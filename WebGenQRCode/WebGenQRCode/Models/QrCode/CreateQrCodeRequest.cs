919d3f182f46752b798b3b6eca1f8444092e6e27
novakvova <novakvova@gmail.com>
2026-09-11 11:30:57 +0300

    Working is good

---
+++ WebGenQRCode/WebGenQRCode/Models/QrCode/CreateQrCodeRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace WebGenQRCode.Models.QrCode;
+
+public class CreateQrCodeRequest
+{
+    //Назва QR-коду
+    public string Name { get; set; } = null!;
+    //Посилання на яке буде здійснено редірект після сканування QR-коду
+    public string TargetUrl { get; set; } = null!;
+}
---
+++ WebGenQRCode/WebGenQRCode/Models/QrCode/QrCodeItemModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WebGenQRCode.Models.QrCode;
+
+public class QrCodeItemModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string TargetUrl { get; set; } = null!;
+    public string Code { get; set; } = null!;
+    public bool IsActive { get; set; }
+    public string CreateAt { get; set; } = null!;
+    public int ScanCount { get; set; }
+}
